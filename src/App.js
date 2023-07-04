44998458a2bbdb4d52a4d3f9b4cb490424506260
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2023-07-04 17:34:10 -0300

    04/07/2023

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  { id: 1, description: "Arroz", un: "Un", quantity: 2, packed: false },
+  { id: 2, description: "Feijão", un: "Un",quantity: 2, packed: false },
+  { id: 3, description: "Macarrão", un: "Un",quantity: 1, packed: true },
+  { id: 4, description: "Carne", un: "Kg",quantity: 2, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -15,14 +22,46 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>Quais serão os itens da compra?</h3>
-    </div>
+      <select>
+        <option value={1} key={1}>1</option>
+        <option value={2} key={2}>2</option>
+        <option value={3} key={3}>3</option>
+        <option value={4} key={4}>4</option>
+        <option value={5} key={5}>5</option>
+      </select>
+      <select>
+        <option value={"Un"}>Un</option>
+        <option value={"Kg"}>Kg</option>
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   )
 }
 
 function PackingList() {
-  return <div className="list">LISTA</div>;
+  return (
+    <div className="list">
+      <ul>
+        {
+          initialItems.map(item => <Item p={item} key={item.id}/>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+function Item ({p}) {
+  return ( 
+    <li>
+      <span style={p.packed ? {textDecoration:"line-through"} : {}}>
+        {p.quantity}{p.un}, {p.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
 }
 
 function Stats() {
